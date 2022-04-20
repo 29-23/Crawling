b2a25d188c8045d1b92f148b23159639d880b060
dayoungss <qbddjehd@naver.com>
2022-04-20 17:03:05 +0900

    added 3 activities

---
+++ app/src/main/java/com/example/personalootd/PersonalRes.java
@@ -0,0 +1,14 @@
+package com.example.personalootd;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PersonalRes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_res);
+    }
+}
