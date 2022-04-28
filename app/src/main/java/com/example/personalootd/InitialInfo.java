92bde410feefdcf91d52f94543062b3befe24aa8
jongho-song <thdwhdgh9@gmail.com>
2022-04-28 20:03:22 +0900

    3번째 페이지 수정

--- app/src/main/java/com/example/personalootd/InitialInfo.java
+++ app/src/main/java/com/example/personalootd/InitialInfo.java
@@ -1,14 +1,32 @@
 package com.example.personalootd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InitialInfo extends AppCompatActivity {
 
+    private Button btn_move; // 다음
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_info);
+
+        btn_move = findViewById(R.id.btn_move);
+
+        //다음 누르면 실행되는 곳 (info -> selfAnalysis)
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InitialInfo.this, SelfAnalysisActivity.class);
+                startActivity(intent); //액티비티 이동
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/personalootd/PersonalRes.java
+++ app/src/main/java/com/example/personalootd/PersonalRes.java
@@ -1,14 +1,45 @@
 package com.example.personalootd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PersonalRes extends AppCompatActivity {
 
+
+    private Button btn_start; // 퍼스널 ootd 시작
+    private Button btn_reAnalysis; // 다시 진단
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_res);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_reAnalysis = findViewById(R.id.btn_reAnalysis);
+
+        //시작 클릭하면 PersonalRes -> MainActivity
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PersonalRes.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        // 다시 진단 클릭하면 PersonalRes -> SelfAnalysisActivity
+        btn_reAnalysis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PersonalRes.this, SelfAnalysisActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
